3372d23709074a41bd8fc79af5933670d84cc92d
karishni <karishinim@gmail.com>
2026-01-28 21:14:57 +0530

    Add files via upload

---
+++ number.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20;
+
+    if (a > b)
+        printf("%d is greater", a);
+    else
+        printf("%d is greater", b);
+
+    return 0;
+}
