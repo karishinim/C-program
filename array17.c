815752a3150ab757dd91ad766fe73cbd34bffa31
karishni <karishinim@gmail.com>
2026-02-04 03:29:12 +0530

    Add files via upload

---
+++ array17.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    int isPalindrome = 1;
+    for(int i = 0; i < n / 2; i++) {
+        if(a[i] != a[n - i - 1]) {
+            isPalindrome = 0;
+            break;
+        }
+    }
+    if(isPalindrome)
+        printf("Yes");
+    else
+        printf("No");
+    return 0;
+}
