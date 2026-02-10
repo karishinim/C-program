fa8a8be6be59a692e08c5e1a6939f89b3eb38ca1
karishni <karishinim@gmail.com>
2026-02-10 09:34:19 +0530

    Add files via upload

---
+++ array2d8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+    int a[R][C];
+    int maxSum = -999999;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < R - 1; i++) {
+        for (int j = 0; j < C - 1; j++) {
+            int sum = a[i][j] + a[i][j+1]
+                    + a[i+1][j] + a[i+1][j+1];
+            if (sum > maxSum)
+                maxSum = sum;
+        }
+    }
+    printf("%d", maxSum);
+    return 0;
+}
