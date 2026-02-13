24a21735ce668bab4500a3ad10b410bae446246c
karishni <karishinim@gmail.com>
2026-02-13 09:42:12 +0530

    Add files via upload

---
+++ array2d25.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+    int a[R][C], sum = 0;
+    for(int i = 0; i < R; i++)
+        for(int j = 0; j < C; j++)
+            scanf("%d", &a[i][j]);
+    for(int j = 0; j < C; j++)
+        sum += a[0][j];
+    for(int j = 0; j < C; j++)
+        sum += a[R-1][j];
+    for(int i = 1; i < R-1; i++) {
+        sum += a[i][0];
+        sum += a[i][C-1];
+    }
+    printf("%d", sum);
+    return 0;
+}
