a228405b144c63b33f5408818364a4747fce7200
karishni <karishinim@gmail.com>
2026-02-15 21:28:17 +0530

    Add files via upload

---
+++ function16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void fibonacci(int n)
+{
+    int a = 0, b = 1, c;
+
+    if (n >= 1)
+        printf("%d ", a);
+    if (n >= 2)
+        printf("%d ", b);
+
+    for (int i = 3; i <= n; i++)
+    {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+}
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    fibonacci(N);
+    return 0;
+}
